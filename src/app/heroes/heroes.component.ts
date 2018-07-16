5599a7c1125fdf1fedd04a26a0aab76842bd0e9c
rwilhel <rwilhel@gmail.com>
2018-07-15 22:38:36 -0600

    Provides click event binding to the heroes template

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,6 +8,13 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
